6b579a3913dbbbbb238e66492e7c2b5d9cfc8ae8
bybyers <byers.jacob@gmail.com>
2026-03-12 18:14:59 -0600

    upldate theme and fonts

--- app/(site)/fonts.ts
+++ app/(site)/fonts.ts
@@ -1,22 +1,22 @@
-import { Sora, Zilla_Slab, Space_Mono } from "next/font/google";
+import { Merriweather_Sans, Merriweather, JetBrains_Mono } from "next/font/google";
 
-export const sans = Sora({
+export const sans = Merriweather_Sans({
   subsets: ['latin'],
   display: 'swap',
   variable: "--font-sans",
-  weight: ['400', '500', '600', '700'],
+  weight: ['300', '400', '500', '600', '700'],
 });
 
-export const serif = Zilla_Slab({
+export const serif = Merriweather({
   subsets: ['latin'],
   display: 'swap',
   variable: "--font-serif",
-  weight: ['400', '500', '600', '700'],
+  weight: ['300', '400', '700'],
 });
 
-export const mono = Space_Mono({
+export const mono = JetBrains_Mono({
   subsets: ['latin'],
   display: 'swap',
   variable: "--font-mono",
-  weight: ['400', '700'],
+  weight: ['400', '500', '600', '700'],
 });
